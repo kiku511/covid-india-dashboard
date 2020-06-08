bc2d858b8c7306daf04fbe4d5c7caaace0fc6285
kiku511 <vansh511@uw.edu>
2020-06-08 03:35:31 -0700

    add map features

--- src/App.js
+++ src/App.js
@@ -1,24 +1,131 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Choropleth from './components/Choropleth';
+import populations from './assets/populations.json';
 
 function App() {
+  const [standard, setStandard] = useState(false);
+  const [selectedState, setSelectedState] = useState('Total');
+  const [data, setData] = useState(null);
+  const [selectedStateData, setSelectedStateData] = useState('null');
+  console.log(selectedState);
+
+  const standardize = (statistic, state) => {
+    if (standard) {
+      let statePopulation = populations[selectedState];
+      return ((statistic / statePopulation) * 1000000).toFixed(2);
+    } else return statistic;
+  };
+
+  const getSelectedData = () => {
+    let stateData = null;
+    if (data) {
+      stateData = data.statewise.filter((feature) => {
+        return feature.state === selectedState;
+      });
+    }
+    return (stateData && stateData[0]) || null;
+  };
+
+  useEffect(() => {
+    fetch('https://api.covid19india.org/data.json')
+      .then((response) => response.json())
+      .then((result) => {
+        setData(result);
+      });
+  }, []);
+
+  useEffect(() => {
+    let stateData = getSelectedData();
+    setSelectedStateData(stateData);
+  });
+
+  let total = null;
+  if (data) {
+    total = data.statewise[0];
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App holy-grail">
+      <header>
+        <h1>COVID-19 India Dashboard</h1>
       </header>
+
+      <div className="holy-grail-body">
+        <section className="holy-grail-content">
+          add legend and scale
+          <Choropleth
+            className="map-area"
+            standard={standard}
+            onChange={setSelectedState}
+            data={data}
+          />
+        </section>
+
+        <div className="holy-grail-sidebar-1 hg-sidebar">
+          <p>Sidebar 1</p>
+          All India Statistics
+          <br />
+          active: {total && standardize(total.active)}
+          <br />
+          confirmed: {total && standardize(total.confirmed)}
+          <br />
+          deaths: {total && standardize(total.deaths)}
+          <br />
+          deltaconfirmed: {total && standardize(total.deltaconfirmed)}
+          <br />
+          recovered: {total && standardize(total.recovered)}
+          <br />
+          deltarecovered: {total && standardize(total.deltarecovered)}
+          <br />
+          lastupdatedtime: {total && total.lastupdatedtime}
+        </div>
+
+        <div className="holy-grail-sidebar-2 hg-sidebar">
+          <p>Sidebar 2</p>
+          {selectedState}
+          <br />
+          active: {selectedStateData && standardize(selectedStateData.active)}
+          <br />
+          confirmed:{' '}
+          {selectedStateData && standardize(selectedStateData.confirmed)}
+          <br />
+          deaths: {selectedStateData && standardize(selectedStateData.deaths)}
+          <br />
+          deltaconfirmed:{' '}
+          {selectedStateData && standardize(selectedStateData.deltaconfirmed)}
+          <br />
+          recovered:{' '}
+          {selectedStateData && standardize(selectedStateData.recovered)}
+          <br />
+          deltarecovered:{' '}
+          {selectedStateData && standardize(selectedStateData.deltarecovered)}
+          <br />
+          lastupdatedtime:{' '}
+          {selectedStateData && selectedStateData.lastupdatedtime}
+          <br />
+          <button type="button" onClick={() => setStandard(false)}>
+            Total Cases
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              console.log('pressed');
+              setStandard(true);
+            }}
+          >
+            Cases Per Million
+          </button>
+        </div>
+      </div>
+
+      <footer>
+        <p>
+          Add references here: population, basemap, shapefile, api, covid19india
+          api
+        </p>
+      </footer>
     </div>
   );
 }
---
+++ src/components/Choropleth.js
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from 'react';
+import { Map, Marker, Popup, TileLayer, GeoJSON } from 'react-leaflet';
+import states from '../assets/india.json';
+import populations from '../assets/populations.json';
+const position = [24.7679, 78.8718];
+
+const Choropleth = (props) => {
+  if (props.data) {
+    console.log(props.data);
+  }
+
+  const getColor = (cases, stateName) => {
+    if (props.standard) {
+      return getStandardColor(cases, stateName);
+    } else return getFlatColor(cases);
+  };
+
+  const getStandardColor = (cases, stateName) => {
+    let statePopulation = populations[stateName];
+    let standard = (cases / statePopulation) * 1000000;
+    return standard > 800
+      ? '#99000d'
+      : standard > 700
+      ? '#cb181d'
+      : standard > 600
+      ? '#ef3b2c'
+      : standard > 500
+      ? '#ef3b2c'
+      : standard > 400
+      ? '#fb6a4a'
+      : standard > 300
+      ? '#fc9272'
+      : standard > 200
+      ? '#fcbba1'
+      : standard > 100
+      ? '#fee0d2'
+      : standard > 0
+      ? '#fff5f0'
+      : '#ffffff';
+  };
+
+  const getFlatColor = (cases) => {
+    return cases > 64000
+      ? '#99000d'
+      : cases > 32000
+      ? '#cb181d'
+      : cases > 16000
+      ? '#ef3b2c'
+      : cases > 8000
+      ? '#ef3b2c'
+      : cases > 4000
+      ? '#fb6a4a'
+      : cases > 2000
+      ? '#fc9272'
+      : cases > 1000
+      ? '#fcbba1'
+      : cases > 500
+      ? '#fee0d2'
+      : cases > 0
+      ? '#fff5f0'
+      : '#ffffff';
+  };
+  const stateColors = (state) => {
+    let stateData = [];
+    if (props.data && props.data.statewise) {
+      stateData = props.data.statewise.filter((feature) => {
+        return feature.state === state.properties.NAME_1;
+      });
+    }
+    if (stateData[0]) {
+      return {
+        fillColor: getColor(stateData[0].confirmed, stateData[0].state),
+        fillOpacity: 0.4,
+        color: '#9E3731',
+        weight: 1.5,
+      };
+    } else return '#FFFFFF';
+  };
+
+  const whenClicked = (event, feature, layer) => {
+    props.onChange(event.sourceTarget.feature.properties.NAME_1);
+  };
+
+  const whenHovered = (feature, layer) => {
+    layer.bringToFront();
+    layer.setStyle({ color: '#FF6D91', weight: 3 });
+  };
+
+  const whenMouseExits = (feature, layer) => {
+    layer.setStyle({ color: '#9e3731', weight: 1.5 });
+  };
+  return (
+    <Map center={position} zoom={5}>
+      <TileLayer
+        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        subdomains="abcd"
+        maxZoom="19"
+      />
+      <GeoJSON
+        data={states}
+        style={stateColors}
+        onEachFeature={(feature, layer) => {
+          layer.on({
+            click: (event) => whenClicked(event, feature, layer),
+            mouseover: () => whenHovered(feature, layer),
+            mouseout: () => whenMouseExits(feature, layer),
+          });
+        }}
+      ></GeoJSON>
+    </Map>
+  );
+};
+
+export default Choropleth;
